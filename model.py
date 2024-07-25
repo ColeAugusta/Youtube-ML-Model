0664fc2411eb9c5955b09ca67873884c73fec814
Cole Augusta <colepaugusta@gmail.com>
2024-07-25 15:06:46 -0400

    Formatting data for model

--- model.py
+++ model.py
@@ -9,25 +9,50 @@ if __name__ == "__main__":
     
     # load trending data
     data = pd.read_csv("csv/US_youtube_trending_data.csv")
-    print(data.info())
 
     # drop unneeded columns from data:
-    # eg. video and channel ids
-    data = data.drop(["video_id", "channel_id", "category_id"], axis=1)
+    # video and channel ids, thumbnail links, etc
+    data = data.drop(["video_id", "title", "publishedAt", "channelId", "channelTitle", "categoryId",
+                       "trending_date", "thumbnail_link"], axis=1)
+
+    # convert needed columns into ints if required
+    # num of tags from tags, comments/rating disabled, etc
+    tag_counts = []
+    for value in data["tags"]:
+        tag_counts.append(value.count('|') + 1)
+    
+    data = data.drop("tags", axis=1)
+    data["tags"] = tag_counts
+
+    comments_disabled = []
+    for value in data["comments_disabled"]:
+        if value == "False":
+            comments_disabled.append(1)
+        else:
+            comments_disabled.append(0)
+
+    data = data.drop("comments_disabled", axis=1)
+    data["comments_disabled"] = comments_disabled
+
+    
+
+    print(data.dtypes)
+
 
     # split into target and feature datasets
     features = data.columns.difference(["view_count"]) 
     X = data[features]
     Y = data["view_count"]
 
+    
     # 80% training, 20% testing split
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.80, random_state=1)
+    #X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.80, random_state=1)
 
     # create model
-    model = LinearRegression()
-    model.fit(X_train, Y_train)
+    #model = LinearRegression()
+    #model.fit(X_train, Y_train)
 
     # use to predict view_count variable
-    predict = model.predict(X_test)
+    #predict = model.predict(X_test)
 
 
